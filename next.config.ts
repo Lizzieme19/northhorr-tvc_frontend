d4883272bebf23bbbb37e5f2dda2b81f88deb572
frank7841 <franklinewax@gmail.com>
2026-07-17 14:04:09 +0300

    Add S3 bucket path patterns to Next.js image config
    - Add specific pathname patterns for news, resources, and documents folders
    - This allows Next.js Image component to load images from S3 bucket
    - Fixes issue where uploaded S3 images were not displaying

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,10 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "plus.unsplash.com" },
       { protocol: "http", hostname: "localhost", port: "5000" },
-      { protocol: "https", hostname: "northhorr.s3.ap-south-1.amazonaws.com"},
+      { protocol: "https", hostname: "northhorr.s3.ap-south-1.amazonaws.com" },
+      { protocol: "https", hostname: "northhorr.s3.ap-south-1.amazonaws.com", pathname: "/news/**" },
+      { protocol: "https", hostname: "northhorr.s3.ap-south-1.amazonaws.com", pathname: "/resources/**" },
+      { protocol: "https", hostname: "northhorr.s3.ap-south-1.amazonaws.com", pathname: "/documents/**" },
     ],
   },
 };
